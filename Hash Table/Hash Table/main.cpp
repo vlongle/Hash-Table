15ad2cc59b430a6c2479eef85dcd3a6cf94efcda
Long Le <vietlong.lenguyen@gmail.com>
2018-03-19 20:14:05 -0400

    Y

--- Hash Table/Hash Table/main.cpp
+++ Hash Table/Hash Table/main.cpp
@@ -1,15 +1,9 @@
-//
-//  main.cpp
-//  Hash Table
-//
-//  Created by Le Nguyen VIet Long on 3/19/18.
-//  Copyright © 2018 Le Nguyen Viet Long. All rights reserved.
-//
-
+// Long Le
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+
+int main(){
+    
+    cout << "Y" << endl;
 }
