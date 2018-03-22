ccf611bf260e58d7a44aff805b786294b656cb88
Long Le <vietlong.lenguyen@gmail.com>
2018-03-22 16:40:24 -0400

    Modify main.cpp

--- Hash Table/Hash Table/main.cpp
+++ Hash Table/Hash Table/main.cpp
@@ -9,45 +9,6 @@
 
 using namespace std;
 
-// Doubly-linked list's Node
-
-struct Data{
-    string name;
-    string value;
-    
-}
-class DLNode{
-    DLNode* right;
-    DLNode* left;
-    Data* data;
-    
-    void insert(DLNode* inNode){
-        // this is a pointer to THIS CLASS
-        inNode->right = this -> right;
-        inNode -> left = this;
-        (this -> right)-> left = inNode;
-        this->right = inNode;  
-    }
-    // delete this Node
-    void delete(){
-         (this -> left)-> right = this-> right;
-          (this -> right) -> left = this -> right;
-        delete this;
-    }
-}
-
-class hashTable{
-    DLNode * firstTableIndex;
-    int const sizeTable;
-    
-    
-    void insert(){
-        
-        
-    }
-    
-}
-
 
 
 int main(){
